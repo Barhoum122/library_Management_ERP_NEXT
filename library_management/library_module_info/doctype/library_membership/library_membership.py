d1df3a034dbb7d608dc225baa63639b144407d1a
Barhoum122 <ibrahimanam122@gmail.com>
2026-08-22 16:59:05 +0000

    Add new Single Doctpe Library Settings

--- library_management/library_module_info/doctype/library_membership/library_membership.py
+++ library_management/library_module_info/doctype/library_membership/library_membership.py
@@ -1,13 +1,12 @@
-# Copyright (c) 2026, faris@example.com and contributors
-# For license information, please see license.txt
+from frappe.model.document import Document
+from frappe.model.docstatus import DocStatus
 
+import frappe
 
-import frappe # type: ignore
-from frappe.model.document import Document # type: ignore
-from frappe.model.docstatus import DocStatus  # type: ignore
 
 class LibraryMembership(Document):
     # check before submitting this document
+    
     def before_submit(self):
         exists = frappe.db.exists(
             "Library Membership",
@@ -20,3 +19,7 @@ class LibraryMembership(Document):
         )
         if exists:
             frappe.throw("There is an active membership for this member")
+
+        # get loan period and compute to_date by adding loan_period to from_date
+        loan_period = frappe.db.get_single_value("Library Settings", "loan_period")
+        self.to_date = frappe.utils.add_days(self.from_date, loan_period or 30)
